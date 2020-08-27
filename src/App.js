e29b948173e6f473ca710d53e9402e89c0ff6d34
sk8beemo <skateor2007@gmail.com>
2020-08-27 17:11:12 +0300

    Styles component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Car from './Car/Car';
 
 class App extends Component {
@@ -34,89 +34,42 @@ class App extends Component {
     this.setState({cars})
   }
 
-  // changeTitleHandler = (newTitle) => {   
-  //   this.setState({
-  //     pageTitle: newTitle
-  //   })
-  // }
-
-  // hanldeInput = (event) => {
-  //   this.setState({
-  //     pageTitle: event.target.value
-  //   })
-  // }
-
   render() {
     const divStyle = {
       textAlign: 'center'
     }
 
-    // let cars = null
+    let cars = null
 
-    // if (this.state.showCars) {
-    //   cars = this.state.cars.map((car, index) => {
-    //     return (
-    //       <Car
-    //         key={index}
-    //         name={car.name}
-    //         year={car.year}
-    //         onChangeTitle={() => this.changeTitleHandler(car.name)}
-    //       />
-    //     )
-    //   })
-    // }
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            name={car.name}
+            year={car.year}
+            onDelete = {this.deleteHandler.bind(this, index)}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
+          />
+        )
+      })
+    }
 
     return (
       <div style={divStyle} className="App">
-        <h1>{this.state.pageTitle}</h1> 
-
-        {/* <input type="text" onChange={this.hanldeInput}></input> */}
+        <h1>{this.state.pageTitle}</h1>
 
         <button
           onClick={this.toggleCarsHandler}
         >Toggle cars</button>
 
-        { this.state.showCars 
-            ? this.state.cars.map((car, index) => {
-              return (
-                <Car
-                  key={index}
-                  name={car.name}
-                  year={car.year}
-                  onDelete = {this.deleteHandler.bind(this, index)}
-                  onChangeName={event => this.onChangeName(event.target.value, index)}
-                />
-              )
-            })
-            : null
-        }
-
-        {/* { this.state.showCars 
-            ? this.state.cars.map((car, index) => {
-              return (
-                <Car
-                  key={index}
-                  name={car.name}
-                  year={car.year}
-                  onChangeTitle={() => this.changeTitleHandler(car.name)}
-                />
-              )
-            })
-            : null
-        } */}
-
-        {/* {cars} */}
-
-        {/* <Car 
-          name={cars[0].name} 
-          year={cars[0].year} 
-          onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
-        />
-        <Car 
-          name={cars[1].name} 
-          year={cars[1].year}
-          onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
-        /> */}
+        <div style={{
+          width: 400,
+          margin: 'auto',
+          paddingTop: '20px'
+        }}>
+          {cars}
+        </div>
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React from 'react'
+import Radium from 'radium'
+import './Car.css'
 
-export default (props) => (
-    <div style={{
-        border: '1px solid gray',
-        margin: '10px auto',
-        width: '200px'
-    }}>
+const car = (props) => {
+
+    const inputClasses = ['input']
+
+    if (props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+
+    const style = {
+        border: '1px solid #ccc',
+        boxShadow : '0 4px 5px 0 rgba(0,0,0,.14)' ,
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 4px 15px 0 rgba(0,0,0, .25)',
+            cursor: 'pointer'
+        }
+    }
+
+    return (
+    <div style={style
+        // border: '1px solid gray',
+        // margin: '10px auto',
+        // display: 'block',
+        // padding: '10px',
+        // boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
+        // borderRadius: '5px'
+    }
+        className="Car"
+    >
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name} />
+        <input 
+            type="text" 
+            onChange={props.onChangeName} 
+            value={props.name} 
+            className={inputClasses.join(' ')}
+        />
         {/* <button onClick={props.onChangeTitle}>Click</button> */}
         <button onClick={props.onDelete}>Delete</button>
     </div>
 )
+}
+
+export default Radium(car)
